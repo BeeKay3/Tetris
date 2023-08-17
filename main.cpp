293ba45e9fbde76f9ee49e5a3af2a3a27fd71665
XRhahelry <sujalbajracharya1@gmail.com>
2023-08-17 07:17:57 +0545

    window created

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<SDL2/SDL.h>
+
+int main(int argc, char * argv[]) {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_SHOWN);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+  SDL_Delay(3000);
+  return 0;
+}
