50a904e3b2287100dde1fa8bb62e60d47fb0d501
XRhahelry <sujalbajracharya1@gmail.com>
2023-08-21 15:29:01 +0545

    game loop setup

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include<SDL2/SDL.h>
-#include<stdio.h>
 
 int main(int argc, char * argv[]) {
-  SDL_Init(SDL_INIT_VIDEO);
   SDL_Window *window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_SHOWN);
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-  SDL_Delay(3000);
+  bool quit = false;
+  SDL_Event e;
+  while (!quit)
+  {
+    if (SDL_PollEvent(&e))
+    {
+      if (e.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+    }
+  }
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
