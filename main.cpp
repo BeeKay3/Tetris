f56a29b80bb8d0e64f408df3e53bfbe052a269ba
XRhahelry <sujalbajracharya1@gmail.com>
2023-10-14 14:32:18 +0545

    input from file

--- main.cpp
+++ main.cpp
@@ -1,21 +1,39 @@
-#include<SDL2/SDL.h>
+#include<iostream>
+#include<fstream>
 
-int main(int argc, char * argv[]) {
-  SDL_Window *window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_SHOWN);
-  bool quit = false;
-  SDL_Event e;
-  while (!quit)
-  {
-    if (SDL_PollEvent(&e))
-    {
-      if (e.type == SDL_QUIT)
-      {
-        quit = true;
+using namespace std;
+
+int main() {
+  fstream init_state;
+
+  init_state.open("./game_states/initial_state.txt", ios::in);
+
+  if (!init_state) {
+    cout << "file no\n";
+  } else {
+    cout << "file yes\n";
+  }
+
+  int grid[20][10], i, j;
+  for(i = 0; i < 20; i++) {
+    for(j = 0; j< 10; j++) {
+      init_state >> grid[i][j];
+      if(init_state.eof()){
+        break;
       }
+    }
+    if(init_state.eof()){
+      break;
     }
   }
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  for (i = 0; i < 20; i++) {
+    for (j = 0; j < 10; j++) {
+      cout << grid[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  init_state.close();
   return 0;
 }
