8a0a441d7def3c3fc996b9450e921c398ab0b2cb
XRhahelry <sujalbajracharya1@gmail.com>
2023-12-06 20:21:32 +0545

    death mechanism added

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-g++ .\src\main.cpp .\src\GetTexture.cpp .\src\Tetromino.cpp .\src\board.cpp --std=c++17 -ISDL2\include\ -LSDL2\lib\ -Wall -Wextra -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -o main
+g++ .\src\main.cpp .\src\GetTexture.cpp .\src\Tetromino.cpp .\src\board.cpp --std=c++17 -ISDL2\include\ -LSDL2\lib\ -Wall -Wextra -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -o main
 .\main.exe
